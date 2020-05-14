6c6468fdb1652a8a423abda0413f2ee934f1f3b9
Katja Stefan <katja.stefan18@gmail.com>
2020-05-14 20:08:16 +0200

    Creating UI class andadding methods

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Book Class: Represents a Book
+td// Book Class: Represents a Book
 class Book {
     constructor(title, author, isbn) {
         this.title = title;
@@ -8,7 +8,41 @@ class Book {
 }
 
 // UI Class: Handles UI Tasks
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Just Kids',
+                author: 'Patti Smith',
+                isbn: '181818'
+            },
+            {
+                title: 'Ciganin, ali najljepsi',
+                author: 'Kristian Novak',
+                isbn: '232323'
+            }
+        ];
 
+        const books = StoredBooks;
+
+        books.forEach(book => UI.addBookToList(book));  
+    }
+
+    static addBookToList(book) {
+        const list = document.getElementById('book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn bth-danger btn-sm delete">X</a></td>
+        `
+        list.appendChild(row);
+        
+    }
+}
 
 // Store Class: Handles Storage
 
