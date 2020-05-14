119871fb38dc92640f45c10e961a366f543c1988
Katja Stefan <katja.stefan18@gmail.com>
2020-05-14 19:54:02 +0200

    Creating a book class

---
+++ app.js
@@ -0,0 +1,19 @@
+// Book Class: Represents a Book
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class: Handles UI Tasks
+
+
+// Store Class: Handles Storage
+
+// Event: Display Books
+
+// Event: Add a Book
+
+// Event: Remove a Book
