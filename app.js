6bfeb1d12331608f058277b887024272bd6becf7
Katja Stefan <katja.stefan18@gmail.com>
2020-05-15 10:26:48 +0200

    Added add a book event

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-td// Book Class: Represents a Book
+// Book Class: Represents a Book
 class Book {
     constructor(title, author, isbn) {
         this.title = title;
@@ -47,7 +47,25 @@ class UI {
 // Store Class: Handles Storage
 
 // Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    // Prevent actual submit
+    e.preventDefault();
+
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+
+    // Instantiate book
+    const book = new Book(title, author, isbn);
+    console.log('book: ', book);
+
+    // Add Book to UI
+    
+});
 
 // Event: Remove a Book
