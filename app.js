869aa468b593def2b7efef5914b707112ed51d0d
Katja Stefan <katja.stefan18@gmail.com>
2020-05-15 11:16:39 +0200

    Added validation in the add book event

--- app.js
+++ app.js
@@ -75,20 +75,24 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
+    // Validate 
+    if(title === '' || author === '' || isbn === '') {
+        alert('Please fill in all fields...');
+    } else {
+        // Instantiate book
+        const book = new Book(title, author, isbn);
+        console.log('book: ', book);
 
-    // Instantiate book
-    const book = new Book(title, author, isbn);
-    console.log('book: ', book);
+        // Add Book to UI
+        UI.addBookToList(book);
 
-    // Add Book to UI
-    UI.addBookToList(book);
-
-    // Clear input fields
-    UI.clearFields();
+        // Clear input fields
+        UI.clearFields();
+        }
+    
 });
 
 // Event: Remove a Book
-
 document.querySelector('#book-list').addEventListener('click', (e) => {
     // e.target is the element in the DOM that we're clicking on
     // console.log(e.target)
